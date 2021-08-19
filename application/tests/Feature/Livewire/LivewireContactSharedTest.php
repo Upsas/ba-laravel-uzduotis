4e8453ced5f6f1dc4c4f1304a56aec088017d15c
Deivydas Brusokas <deivydas.brusokas@gmail.com>
2021-08-19 12:46:15 +0300

    Livewire tests

---
+++ application/tests/Feature/Livewire/LivewireContactSharedTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use App\Http\Livewire\ContactShared;
+use App\Models\Contact;
+use App\Models\SharedContact;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class LivewireContactSharedTest extends TestCase
+{
+    use LivewireTestHelper;
+
+    private Collection|Model $user;
+    private Collection|Model $contact;
+    private int $contactId;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = $this->actAsUser();
+        $this->contact = $this->createContact();
+        $this->contactId = $this->contact->getAttribute('id');
+    }
+
+
+    public function testCancelModalIsShown(): void
+    {
+        Livewire::test(ContactShared::class)
+            ->call('cancel', $this->contactId)
+            ->assertSet('showModal', true)
+            ->assertSet('contactId', $this->contactId)
+            ->assertSeeHtml('Stop');
+
+    }
+
+    public function testDeleteModalIsShown(): void
+    {
+
+        $message = 'Are you sure want to delete shared contact with you?';
+        Livewire::test(ContactShared::class)
+            ->call('deleteShared', $this->contactId)
+            ->assertSet('message', $message)
+            ->assertSet('buttonName', 'Yes')
+            ->assertSet('contactId', $this->contactId)
+            ->assertSet('showModal', true)
+            ->assertSeeHtml($message);
+    }
+
+    public function testCanDeleteSharedContact(): void
+    {
+
+        $newUser = $this->actAsUser();
+        $contact = $this->createContact();
+        $sharedContact = SharedContact::create([
+            'user_id' => Auth::id(),
+            'contact_id' => $contact->getAttribute('id'),
+            'contact_shared_user_id' => $newUser->getAttribute('id')
+        ]);
+
+        Livewire::test(ContactShared::class)
+            ->set('contactId', $sharedContact->getAttribute('id'))
+            ->call('delete')
+            ->assertEmitted('flashMessage');
+
+        $this->assertDeleted($sharedContact);
+
+        Contact::where('user_id', Auth::id())->first()->delete();
+        $this->deleteContactAndUser($contact, $newUser);
+    }
+
+    public function testCanAddSharedContact(): void
+    {
+        $newUser = User::factory()->create();
+        $contact = (Contact::factory(
+            [
+                'name' => 'testas',
+                'number' => 8954,
+                'user_id' => $newUser->getAttribute('id')
+            ])->create());
+        $sharedContact = $this->createSharedContact($newUser, $contact);
+
+        Livewire::test(ContactShared::class)
+            ->call('addContact', $contact->id,
+                $sharedContact->getAttribute('id'))
+            ->assertEmitted('flashMessage');
+
+        $this->assertTrue(Contact::where('user_id', Auth::id())->first()->exists());
+        $this->assertDeleted($sharedContact);
+        Contact::destroy($contact->id);
+        User::destroy($newUser->id);
+        Contact::where('user_id', Auth::id())->first()->delete();
+    }
+
+    public function tearDown(): void
+    {
+        $this->deleteContactAndUser($this->contact, $this->user);
+    }
+
+}
--- application/tests/Feature/Livewire/LivewireTestHelper.php
+++ application/tests/Feature/Livewire/LivewireTestHelper.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Livewire;
 
 
 use App\Models\Contact;
+use App\Models\SharedContact;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Auth;
 
 trait LivewireTestHelper
 {
+
 
     public function createContact(): Model|Collection
     {
@@ -28,6 +30,23 @@ trait LivewireTestHelper
         $newUser = User::factory()->create();
         $this->actingAs($newUser);
         return $newUser;
+
+    }
+
+    public function createSharedContact(Model $newUser, Model $contact): Model
+    {
+
+        return SharedContact::create([
+            'user_id' => $newUser->getAttribute('id'),
+            'contact_id' => $contact->getAttribute('id'),
+            'contact_shared_user_id' => Auth::id()
+        ]);
+    }
+
+    public function deleteSharedContact(Model $sharedContact): void
+    {
+
+        SharedContact::destroy($sharedContact->getAttribute('id'));
 
     }
 
